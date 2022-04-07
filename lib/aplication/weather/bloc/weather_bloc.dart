aa1fe597e978893a299f22bf57b5b149b8f5b1c4
rafiyandi <rafiyandi1905@gmail.com>
2022-04-07 09:21:26 +0700

    domain, infrastruktur dan bloc

--- lib/aplication/weather/bloc/weather_bloc.dart
+++ lib/aplication/weather/bloc/weather_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:dartz/dartz.dart';
+import 'package:forecasting/domain/forecasting_weather_interface.dart';
 import 'package:forecasting/domain/forecasting_weather_response.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
@@ -8,7 +9,9 @@ part 'weather_state.dart';
 part 'weather_bloc.freezed.dart';
 
 class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
-  WeatherBloc() : super(_Initial());
+  WeatherBloc(this._forecastingWeatherInterface) : super(_Initial());
+
+  ForecastingWeatherInterface _forecastingWeatherInterface;
 
   Stream<WeatherState> mapEventToState(
     WeatherEvent event,
@@ -18,6 +21,10 @@ class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
         getMainData: (e) async* {
           yield WeatherState.mainDataOptions(
               onLoading: true, weatherData: none());
+
+          final _result = await _forecastingWeatherInterface.getAllDataMain();
+          yield WeatherState.mainDataOptions(
+              onLoading: false, weatherData: some(_result));
         },
         getCityData: (e) async* {});
   }
